21e3251d866c2379a1a8fba457b7217889ae2821
maxjvjohansson <max.jv.johansson@gmail.com>
2025-02-20 16:05:31 +0100

    Removed test-project from Git tracking and added to .gitignore

--- test-project/assets/js/app.js
+++
@@ -1,3 +0,0 @@
-fetch('../images/logo.png')  // ❌ Absolut sökväg
-    .then(response => response.blob())
-    .then(data => console.log("Image loaded", data));
--- test-project/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <p>&copy; 2025 Test Project</p>
-</footer>
-
-<script src="../assets/js/app.js"></script> <!-- ❌ Absolut sökväg -->
-<script src="../assets/js/validation.js"></script> <!-- ❌ Absolut sökväg -->
-</body>
-
-</html>
